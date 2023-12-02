e14a4882c82c19b8bc0ad3d396d3caef18e01f29
EndaMagennis <>
2023-12-02 15:16:48 +0000

    Fix:  app configurations

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,20 +1,11 @@
-# Import the os module to interact with the operating system
 import os
-
-# Import Flask to create an instance of the Flask web application
 from flask import Flask
-
-# Import SQLAlchemy to interact with the database
 from flask_sqlalchemy import SQLAlchemy
-
-# Check if the env.py file exists and import it if it does
 if os.path.exists("env.py"):
-    import env
+    import env  # noqa
 
-# Create an instance of Flask and store it in a variable called app
+
 app = Flask(__name__)
-
-# Configure the secret key and the database URI for the Flask application
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
@@ -23,8 +14,7 @@ else:
     if uri.startswith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://", 1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri
-# Create an instance of SQLAlchemy and store it in a variable called db
-db = SQLAlchemy(app)
 
+db = SQLAlchemy(app)
 
 from taskmanager import routes  # noqa
